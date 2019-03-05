1d9b4be3456c17a649355e668be5a3dd0a29139a
Kevin Lu <kevinyihchyunlu@gmail.com>
2019-03-05 16:51:55 +1100

    implemented base class for loss wrappers
    Former-commit-id: bf7f362bac745bbd477a32045b8cdee352ae3303
    Former-commit-id: 45122a3b883697d68cbf55691af8bc6fa3fe1723
    Former-commit-id: 4c094d71ea9046cea79a252bafbf21dbbf5b26dc

--- pytorch_toolbox/fastai_extensions/loss/core.py
+++ pytorch_toolbox/fastai_extensions/loss/core.py
@@ -1,3 +1,4 @@
+import abc
 from collections import defaultdict
 
 import torch
@@ -10,6 +11,134 @@ import fastai
 
 
 # TODO:  Have a super class which wraps all the losses so that we can easily get different ways that the loss can be reduced
+
+
+class BaseLoss:
+
+    @staticmethod
+    def reshape_to_batch_x_minus_one_and_sum_over_last_dimension(tensor):
+        batch_size = tensor.size(0)
+        return tensor.view(batch_size, -1).sum(dim=1)
+
+    @abc.abstractmethod
+    @property
+    def unreduced_loss(self):
+        pass
+
+    @abc.abstractmethod
+    @property
+    def per_sample_loss(self):
+        pass
+
+    @abc.abstractmethod
+    @property
+    def reduced_loss(self):
+        pass
+
+
+class FocalLoss(BaseLoss):
+    def __init__(self, gamma=2):
+        self.gamma = gamma
+
+    @property
+    def unreduced_loss(self):
+        return self._unreduced_focal_loss
+
+    @property
+    def per_sample_loss(self):
+        return self._per_sample_loss
+
+    @property
+    def reduced_loss(self):
+        return self._reduced_loss
+
+    def __call__(self, out, *yb):
+        prediction = out
+        target = yb[0]
+        # This returns B x ... (same shape as input)
+        self._unreduced_focal_loss = calculate_focal_loss(prediction, target, self.gamma)
+        self._per_sample_loss = self.reshape_to_batch_x_minus_one_and_sum_over_last_dimension(
+            self._unreduced_focal_loss)
+        self._reduced_loss = self._per_sample_loss.mean()
+        return self._reduced_loss
+
+
+def calculate_focal_loss(input, target, gamma=2):
+    """
+
+    :param input: B x N_classes for classification, or B x H x W for segmentation
+    :param target: B x N_classes for classification, or B x H x W for segmentation
+    :param gamma: the higher the value, the greater the loss for uncertain classes
+    :return:
+    """
+    if not (target.size() == input.size()):
+        raise ValueError("Target size ({}) must be the same as input size ({})"
+                         .format(target.size(), input.size()))
+
+    max_val = (-input).clamp(min=0)
+    loss = input - input * target + max_val + \
+           ((-max_val).exp() + (-input - max_val).exp()).log()
+
+    invprobs = F.logsigmoid(-input * (target * 2.0 - 1.0))
+    loss = (invprobs * gamma).exp() * loss
+    return loss
+
+
+class SoftF1Loss:
+    def __call__(self, out, *yb):
+        prediction = out
+        target = yb[0]
+
+        # This returns B x n_classes
+        f1_soft_loss = calculate_f1_soft_loss(prediction, target)
+        self.per_sample_loss = f1_soft_loss
+        self.loss = f1_soft_loss
+        return f1_soft_loss.mean()
+
+
+def calculate_f1_soft_loss(logits, labels):
+    """
+    logits: B x N_classes
+    labels: B x N_classes
+    """
+
+    __small_value = 1e-6
+    beta = 1
+    probs = F.sigmoid(logits)
+    soft_tp_plus_fp = torch.sum(probs, 1) + __small_value
+    # note that this is a bit of a misnomer as the labels are usually 1 hot, but for the case that they aren't this
+    # would make a bit more sense
+    soft_tp_plus_fn = torch.sum(labels, 1) + __small_value
+    true_positive = torch.sum(labels * probs, 1)
+    precision = true_positive / soft_tp_plus_fp
+    recall = true_positive / soft_tp_plus_fn
+    f1_soft = (1 + beta * beta) * precision * recall / (beta * beta * precision + recall + __small_value)
+    return 1 - f1_soft
+
+
+class LovaszHingeFlatLoss:
+    def __call__(self, out, *yb):
+        prediction = out
+        target = yb[0]
+        original_shape = target.shape
+        lovasz_loss = lovasz_hinge_flat(prediction.flatten(), target.flatten(), reduce=False)
+        self.per_sample_loss = lovasz_loss.view(*original_shape).sum(dim=1)
+        self.loss = lovasz_loss.view(*original_shape)
+        return lovasz_loss.view(*original_shape).sum(dim=1).mean()
+
+
+class LossWrapper(nn.Module):
+    def __init__(self, losses):
+        super().__init__()
+        for loss in losses:
+            setattr(self, fastai.camel2snake(loss.__class__.__name__), loss)
+        self.losses = losses
+
+    def forward(self, out, *yb):
+        total_loss = sum([l(out, *yb) for l in self.losses])
+        return total_loss
+
+
 class SoftDiceLoss:
     def __init__(self, dice_loss_weights=None, n_classes=2):
         assert n_classes > 1, "Even if it is a binary classification, please use 2 classes instead of one"
@@ -35,60 +164,6 @@ class CELoss:
         ce_loss = calculate_ce_loss(prediction, target)
         self.loss = ce_loss
         return torch.mean(ce_loss)
-
-
-class FocalLoss:
-    def __init__(self, gamma=2):
-        self.gamma = gamma
-
-    @staticmethod
-    def sum_over_last_dimension(tensor):
-        batch_size = tensor.size(0)
-        return tensor.view(batch_size, -1).sum(dim=1)
-
-    def __call__(self, out, *yb):
-        prediction = out
-        target = yb[0]
-        # This returns B x ... (same shape as input)
-        unreduced_focal_loss = calculate_focal_loss(prediction, target, self.gamma)
-        self.loss = unreduced_focal_loss
-        self.per_sample_loss = self.sum_over_last_dimension(unreduced_focal_loss)
-        return self.sum_over_last_dimension(unreduced_focal_loss).mean()
-
-
-class SoftF1Loss:
-    def __call__(self, out, *yb):
-        prediction = out
-        target = yb[0]
-
-        # This returns B x n_classes
-        f1_soft_loss = calculate_f1_soft_loss(prediction, target)
-        self.per_sample_loss = f1_soft_loss
-        self.loss = f1_soft_loss
-        return f1_soft_loss.mean()
-
-
-class LovaszHingeFlatLoss:
-    def __call__(self, out, *yb):
-        prediction = out
-        target = yb[0]
-        original_shape = target.shape
-        lovasz_loss = lovasz_hinge_flat(prediction.flatten(), target.flatten(), reduce=False)
-        self.per_sample_loss = lovasz_loss.view(*original_shape).sum(dim=1)
-        self.loss = lovasz_loss.view(*original_shape)
-        return lovasz_loss.view(*original_shape).sum(dim=1).mean()
-
-
-class LossWrapper(nn.Module):
-    def __init__(self, losses):
-        super().__init__()
-        for loss in losses:
-            setattr(self, fastai.camel2snake(loss.__class__.__name__), loss)
-        self.losses = losses
-
-    def forward(self, out, *yb):
-        total_loss = sum([l(out, *yb) for l in self.losses])
-        return total_loss
 
 
 def calculate_ce_loss(preds, targets, weight_maps=None, weight=1):
@@ -153,44 +228,3 @@ def dice_loss(inputs, one_hots):
     one_hots_flat = one_hots.contiguous().view(-1)
     intersection = torch.sum(inputs_flat * one_hots_flat)
     return 1 - ((2 * intersection + 1.) / (torch.sum(inputs_flat) + torch.sum(one_hots_flat) + 1.))
-
-
-def calculate_focal_loss(input, target, gamma=2):
-    """
-
-    :param input: B x N_classes
-    :param target: B x N_classes
-    :param gamma: the higher the value, the greater the loss for uncertain classes
-    :return:
-    """
-    if not (target.size() == input.size()):
-        raise ValueError("Target size ({}) must be the same as input size ({})"
-                         .format(target.size(), input.size()))
-
-    max_val = (-input).clamp(min=0)
-    loss = input - input * target + max_val + \
-           ((-max_val).exp() + (-input - max_val).exp()).log()
-
-    invprobs = F.logsigmoid(-input * (target * 2.0 - 1.0))
-    loss = (invprobs * gamma).exp() * loss
-    return loss
-
-
-def calculate_f1_soft_loss(logits, labels):
-    """
-    logits: B x N_classes
-    labels: B x N_classes
-    """
-
-    __small_value = 1e-6
-    beta = 1
-    probs = F.sigmoid(logits)
-    soft_tp_plus_fp = torch.sum(probs, 1) + __small_value
-    # note that this is a bit of a misnomer as the labels are usually 1 hot, but for the case that they aren't this
-    # would make a bit more sense
-    soft_tp_plus_fn = torch.sum(labels, 1) + __small_value
-    true_positive = torch.sum(labels * probs, 1)
-    precision = true_positive / soft_tp_plus_fp
-    recall = true_positive / soft_tp_plus_fn
-    f1_soft = (1 + beta * beta) * precision * recall / (beta * beta * precision + recall + __small_value)
-    return 1 - f1_soft
