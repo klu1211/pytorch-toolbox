fixed metric calculation for soft f score loss
Former-commit-id: dd3d5ff067f631e31a1446521c3aa75d00decb66
Former-commit-id: 2dc87ef5b0c866675b51322c0cc8f85f8d2b9826
Former-commit-id: 2b2a28e2033f05ce47061316f1e11b4899c97665

pytorch_toolbox/core/metrics/soft_f_score.py
@@ -9,7 +9,7 @@ def sigmoid_np(x):
     return 1.0 / (1.0 + np.exp(-x))
 
 
-def f_score_soft(preds, targs, beta, th=0.5, d=50.0):
+def soft_f_score(preds, targs, beta, th=0.5, d=50.0):
     if isinstance(preds, torch.Tensor) and isinstance(targs, torch.Tensor):
         return (-1. * calculate_soft_f_score_loss(preds, targs, beta=beta).mean()) + 1
     else:

pytorch_toolbox/core/training/learner/core.py
@@ -7,6 +7,7 @@ from torch.utils.data import DataLoader
 from torch.optim import Adam
 import numpy as np
 from fastprogress import progress_bar, master_bar
+from miniutils import progbar
 
 from pytorch_toolbox.core.training.learner.train import fit_one_cycle, lr_find, to_fp16
 from pytorch_toolbox.core.callbacks import Callback, CallbackList, CallbackHandler, Recorder
@@ -81,12 +82,12 @@ class Learner:
         assert dl is not None
         metrics = if_none(metrics, self.metrics)
         callbacks_fns = [cb(self) for cb in if_none(callback_fns, [])]
-        cb_handler = callbacks.CallbackHandler(self.callbacks + if_none(callbacks, []) + callbacks_fns, metrics)
+        cb_handler = CallbackHandler(self.callbacks + if_none(callbacks, []) + callbacks_fns, metrics)
         with torch.no_grad():
             self.model.eval()
-            for xb, yb in progress_bar(dl, parent=pbar, leave=(pbar is not None)):
+            for xb, yb in progbar(dl):
                 if cb_handler: xb, yb = cb_handler.on_batch_begin(xb, yb, train=False)
-                cb_handler = if_none(cb_handler, callbacks.CallbackHandler())
+                cb_handler = if_none(cb_handler, CallbackHandler())
                 if not is_listy(xb):
                     xb = [xb]
                 out = self.model(*xb)