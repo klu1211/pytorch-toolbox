137011d539ef5d774c5cf83852c6472766d4d4fe
Kevin Lu <kevinyihchyunlu@gmail.com>
2019-05-03 11:41:23 +1000

    added method to create pipeline from configuration file
    Former-commit-id: 825c22ec8a600ab2b3d68b64e499a352bd298427
    Former-commit-id: ade4090f1ce89e0aef7839e92811acc546b4abb3
    Former-commit-id: 8d79dfa4f8463090c44aef756e27b3e6cd405ae7

--- core/pipeline/yaml_loader.py
+++ core/pipeline/yaml_loader.py
@@ -1,21 +1,49 @@
+from pathlib import Path
 import yaml
 from copy import deepcopy
 from dataclasses import dataclass
 
 
 class PyTorchToolboxLoader(yaml.SafeLoader):
-    @staticmethod
-    def replace_config_variables(config):
-        return replace_config_variables(config)
+    pass
 
 
-def var_constructor(loader, node):
-    return Variable(name=node.value)
+class PyTorchToolboxDumper(yaml.SafeDumper):
+    pass
 
 
 @dataclass
 class Variable:
-    name: str
+    node_name: str
+    variable_name: str
+
+    @property
+    def is_nested(self):
+        return self.node_name is not None
+
+
+def var_constructor(loader, node):
+    if "." in node.value:
+        node_name_and_variable_name = node.value.split(".")
+        assert len(node_name_and_variable_name) == 2
+        node_name, variable_name = node_name_and_variable_name
+        return Variable(node_name=node_name, variable_name=variable_name)
+    else:
+        return Variable(node_name=None, variable_name=node.value)
+
+
+def var_representer(dumper, data):
+    if data.is_nested:
+        value = f"{data.node_name}.{data.variable_name}"
+    else:
+        value = f"{data.variable_name}"
+    return dumper.represent_scalar('!Var', f"{value}")
+
+
+@dataclass
+class Reference:
+    ref_node_name: str
+    output_name: str
 
 
 def ref_constructor(loader, node):
@@ -25,13 +53,12 @@ def ref_constructor(loader, node):
     return Reference(ref_node_name=ref_node_name, output_name=output_name)
 
 
-@dataclass
-class Reference:
-    ref_node_name: str
-    output_name: str
+def ref_representer(dumper, data):
+    value = f"{data.ref_node_name}.{data.output_name}"
+    return dumper.represent_scalar('!Var', f"{value}")
 
 
-def replace_config_variables(config, resource_key="Resources", variable_key="Variables"):
+def replace_config_variables(config, resource_key, variable_key):
     config = deepcopy(config)
     replaced_resources = replace_variables(config[resource_key], config[variable_key])
     config[resource_key] = replaced_resources
@@ -46,27 +73,40 @@ def replace_variables(resources, variables):
         for i, resource in enumerate(resources):
             resources[i] = replace_variables(resource, variables)
     elif isinstance(resources, Variable):
-        if "." in resources.name:
+        # name reassignment to make intent clearer that this resource is a variable
+        if resources.is_nested:
             resources = nested_variable_replacement(resources, variables)
         else:
-            resources = variables[resources.name]
+            resources = variables[resources.variable_name]
     else:
         return resources
     return resources
 
 
 def nested_variable_replacement(resource, variables):
-    resource_name_and_variable_name = resource.name.split(".")
-    assert len(
-        resource_name_and_variable_name) == 2, f"{resource.name} is not in the correct format, there can only be one '.'"
-    resource_name, variable_name = resource_name_and_variable_name
-    return variables[resource_name][variable_name]
+    return variables[resource.node_name][resource.variable_name]
 
 
-# This tells the loader that when it sees "!Var"
-# it will pass the value proceeding the !path value into the var constructor
 PyTorchToolboxLoader.add_constructor('!Var', var_constructor)
+PyTorchToolboxDumper.add_representer(Variable, var_representer)
 
-# This tells the loader that when it sees "!Ref"
-# it will pass the value proceeding the !path value into the var constructor
 PyTorchToolboxLoader.add_constructor('!Ref', ref_constructor)
+PyTorchToolboxDumper.add_representer(Reference, ref_representer)
+
+
+def load_config_from_string(config_string, with_variable_replacement=False):
+    config = yaml.load(config_string, Loader=PyTorchToolboxLoader)
+    if with_variable_replacement:
+        config = replace_config_variables(config)
+    return config
+
+
+def load_config_from_path(path, with_variable_replacement=False):
+    with Path(path).open("r") as f:
+        config = load_config_from_string(f, with_variable_replacement)
+    return config
+
+
+def save_config(config, save_path):
+    with save_path.open('w') as yaml_file:
+        yaml.dump(config, yaml_file, Dumper=PyTorchToolboxDumper, default_flow_style=False)
