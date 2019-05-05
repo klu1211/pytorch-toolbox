960bacbc439afac9d23c3a62bf99b7ea058a7b58
Kevin Lu <kevinyihchyunlu@gmail.com>
2019-05-05 11:25:42 +1000

    renamed directory containing pytorch_toolbox to avoid double imports
    Former-commit-id: a44734b885f4eb74bd9f4694521a2754cff3bb42
    Former-commit-id: 0a648ab7d13f4f0a78f4d9d422826381f1ebf3c2
    Former-commit-id: 59523dd3c6df1e07d069dc27e3b28c42d389f6cf

--- src/scripts/download_and_preprocess_data.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-export PYTHONPATH=..
-python scripts/kaggle_data_download.py
-python scripts/external_data_download.py
-python scripts/image_merging.py
-python scripts/data_deduplication.py
--- src/scripts/train.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-python train_with_template.py -cfg configs/densenet121_two_input_fc_with_tta_template_docker.yml
