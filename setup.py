e0393a63a4cd2999aa1dd35175f34b730178f18a
Kevin Lu <kevinyihchyunlu@gmail.com>
2019-06-13 12:22:14 +1000

    updated version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='pytorch_toolbox',
-      version='0.0.1',
+      version='0.1.0',
       description='A PyTorch Toolbox',
       url='https://github.com/kevinlu1211/pytorch-toolbox',
       author='Kevin Lu',
