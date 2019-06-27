3144190221019256ebce77a6239c6b5329ede89a
Kevin Lu <kevinyihchyunlu@gmail.com>
2019-06-27 13:29:56 +1000

    added imports for refactored u net weight map file

--- pytorch_toolbox/utils/vision/__init__.py
+++ pytorch_toolbox/utils/vision/__init__.py
@@ -1,3 +1,4 @@
-from .utils import *
+from .utils import normalize_fn_lookup, denormalize_fn_lookup
+from .u_net_weight_map import create_u_net_weight_map
 
 lookups = {**normalize_fn_lookup, **denormalize_fn_lookup}
