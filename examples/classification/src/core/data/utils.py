5141f8bb25b55d03f396a8108d531a0a170f4815
Kevin Lu <kevinyihchyunlu@gmail.com>
2019-07-23 17:06:51 +1000

    added ClassificationImageSample to wrap Image and Label classes

--- examples/classification/src/core/data/utils.py
+++ examples/classification/src/core/data/utils.py
@@ -1,8 +1,29 @@
 from functools import partial
 
 import cv2
+from abc import ABC, abstractmethod
 import torch
 import numpy as np
+
+
+class ClassificationImageSample:
+    def __init__(self, image, target):
+        self.image = image
+        self.target = target
+
+    @classmethod
+    def create(
+        cls,
+        image_data=None,
+        image_path=None,
+        load_image_fn=partial(cv2.imread, cv2.IMREAD_UNCHANGED),
+        label_data=None,
+        label_path=None,
+        load_label_fn=np.load,
+    ):
+        image = Image(image_data, image_path, load_image_fn)
+        label = Label(label_data, label_path, load_label_fn)
+        return cls(image, label)
 
 
 class Image:
